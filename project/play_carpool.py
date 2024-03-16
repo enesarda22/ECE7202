5bbd10804ebe4ef39eaea02c17d221b15f98fa4a
enesarda <enesarda22@gmail.com>
2024-03-16 18:05:27 -0400

    rename files

--- project/play_carpool.py
+++ project/play_carpool.py
@@ -3,7 +3,7 @@ import time
 import gymnasium as gym
 import torch
 
-from project.main import DQN
+from project.train_dqn import DQN
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
